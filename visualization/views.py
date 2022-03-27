7be086fd2dd8cecdc4d441d614a65cb3f2135853
RehnumaRatul <85509899+RehnumaRatul@users.noreply.github.com>
2022-03-27 16:38:11 +0600

    Update views.py

--- visualization/views.py
+++ visualization/views.py
@@ -2,6 +2,7 @@ from tkinter.tix import Form
 from django.shortcuts import redirect, render
 from .models import VisualData
 from .forms import VisualDataFrom
+from django.contrib import messages
 
 
 
@@ -13,9 +14,29 @@ text = serializers.serialize("python",VisualData.objects.all())
 
 # Create your views here.
 
+def delete(request,id):
+        data = VisualData.objects.get(id=id)
+        data.delete()
+        return redirect('/')
+
+def update(request,id):
+        data = VisualData.objects.get(id=id)
+        form= VisualDataFrom(request.POST, instance=data)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record updated successfully...!")
+            return render(request, 'visualization/edit.html', {VisualData:data })
+
+
+def edit(request,id):
+        data = VisualData.objects.get(id=id)
+       
+        return render(request, 'visualization/edit.html', {VisualData:data })
 
 
 
+       
+        
  
 def index(request):
     a = 27
@@ -37,3 +58,4 @@ def index(request):
         
     }
     return render(request, 'visualization/index.html', context)
+    
