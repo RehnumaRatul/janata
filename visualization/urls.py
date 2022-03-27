ff9024dc198617fd17dea886dfed1ba1623a9952
RehnumaRatul <85509899+RehnumaRatul@users.noreply.github.com>
2022-03-27 16:39:03 +0600

    Update urls.py

--- visualization/urls.py
+++ visualization/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='visualization-index'),
+    path('delete/<int:id>', views.delete),
+    path('edit/<int:id>', views.edit),
+    path('update/<int:id>', views.update)
 ]
